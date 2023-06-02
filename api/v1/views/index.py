a915138179f9d3876c3e394f2cd02933b8612026
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-06-02 21:27:25 +0100

    retrieve objs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,21 @@ def get_status():
     shows the status
     """
     return jsonify({"status": "OK"})
+
+
+# Create a route '/stats' on the app_views object that
+# returns the count of each object type
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """
+    Retrieves the number of each objects
+    """
+    objs = {
+            "amenities": storage.count(Amenities),
+            "cities": storage.count(Cities),
+            "places": storage.count(Place),
+            "reviews": storage.count(Reviews),
+            "states": storage.count(States),
+            "users": storage.count(Users)
+            }
+    return jsonify(objs)
