99a485c29f5e8e53cf38b7ac7a2d73517fca6af9
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-06-02 21:16:26 +0100

    add doc

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""
+Python package
+"""
+
 from flask import Blueprint
-# Wildcard import of everything in the package api.v1.views.index
-from api.v1.views.index import *
 
-
-# Create a variable app_views which is an instance of
-# Blueprint with a url prefix of '/api/v1'
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+contains the end route status
+"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 # Create a route '/status' on the app_views object that returns a JSON response
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """
+    shows the status
+    """
     return jsonify({"status": "OK"})
