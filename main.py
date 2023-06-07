db35b56b9096c62af2cb4b124044a49f0e845680
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-06-07 18:52:19 +0100

    add file

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 """Testing file
 """
-import hashlib
-import MySQLdb
-import sys
+import json
+import requests
 
 if __name__ == "__main__":
-    """ Access to database and get password TODO
+    """ POST /api/v1/places_search
     """
-    user_email = "b@b.com"
-    clear_pwd = "pwdB"
-    hidden_pwd = hashlib.md5(clear_pwd.encode()).hexdigest()
-    
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT password FROM users WHERE email = '{}' LIMIT 1".format(user_email))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if hidden_pwd.lower() == row[0].lower():
-            print("OK")
-    cur.close()
-    conn.close()
+    r = requests.post("http://0.0.0.0:5050/api/v1/places_search", data=json.dumps({}), headers={ 'Content-Type': "application/json" })
+    r_j = r.json()
+    print(type(r_j))
+    print(len(r_j))
